dd8431f97333afc629fe8e7bf9231f54e1dd5022
Erwin Junge <erwin@junge.nl>
2018-06-05 15:06:49 +0200

    Update charts, use mode for readability

--- charts.py
+++ charts.py
@@ -9,6 +9,37 @@ from models import (
 )
 from engine import engine
 import pygal
+from collections import Counter
+
+
+def round_to_interval(number, interval):
+    return (
+        (interval * int(number / interval))
+        + (interval / 2)
+    )
+
+
+def get_mode(list_like, bin_size):
+    binned_list = [
+        round_to_interval(item, bin_size)
+        for item in list_like
+    ]
+    return Counter(binned_list).most_common(1)[0][0]
+
+
+def chop_list(list_like, step_sizes):
+    return [
+        list_like[sum(step_sizes[:i]):sum(step_sizes[:i+1])]
+        for i in range(len(step_sizes))
+    ]
+
+
+def get_step_sizes(total_length, number_of_steps):
+    step_sizes = [int(total_length / number_of_steps)] * number_of_steps
+    remainder = total_length - sum(step_sizes)
+    for i in range(remainder):
+        step_sizes[i] += 1
+    return step_sizes
 
 
 def get_measurement_times(session):
@@ -20,7 +51,7 @@ def get_measurement_times(session):
     return [obj.created_at for obj in times_query]
 
 
-def create_cpu_chart(session, measurement_times):
+def create_cpu_chart(session, measurement_times, steps):
     query = (
         session
         .query(
@@ -40,30 +71,42 @@ def create_cpu_chart(session, measurement_times):
         x_label_rotation=20,
         x_labels_major_count=10,
         show_minor_x_labels=False,
+        show_dots=False,
     )
+    step_sizes = get_step_sizes(len(measurement_times), steps)
+    mid_points = [
+        sum(step_sizes[:i]) + int(step_sizes[i]/2)
+        for i in range(len(step_sizes))
+    ]
     chart.x_labels = [
-        measurement_time.isoformat()
-        for measurement_time
-        in measurement_times
+        measurement_times[i].isoformat()
+        for i in mid_points
     ]
     for cpu_index, cpu_measurements in measurements.items():
+        data = [
+            cpu_measurements.get(measurement_time, 0)
+            for measurement_time
+            in measurement_times
+        ]
+        chopped_data = chop_list(data, step_sizes)
+        modes = [
+            get_mode(part, 2)
+            for part in chopped_data
+        ]
         chart.add(
             '{}'.format(cpu_index),
-            [
-                cpu_measurements.get(measurement_time, None)
-                for measurement_time
-                in measurement_times
-            ],
+            modes,
             allow_interruptions=True,
         )
     chart.render_to_file('./cpu_chart.svg')
+    chart.render_to_png('./cpu_chart.png')
 
 
-def create_process_chart(session, measurement_times):
-    interesting_names = (
+def create_process_chart(session, measurement_times, steps):
+    interesting_cmdlines = (
         session
-        .query(Process.name)
-        .group_by(Process.name)
+        .query(Process.cmdline)
+        .group_by(Process.cmdline)
         .order_by(desc(func.sum(Process.cpu_percent)))
         .slice(0, 5)
     )
@@ -71,47 +114,62 @@ def create_process_chart(session, measurement_times):
         session
         .query(
             Measurement.created_at,
-            Process.name,
+            Process.cmdline,
             Process.cpu_percent,
         )
         .filter(
-            Process.name.in_(interesting_names),
+            Process.cmdline.in_(interesting_cmdlines),
             Measurement.id == Process.measurement_id,
         )
     )
     measurements = {}
-    for measurement_time, name, cpu_percent in process_query:
-        cpu_measurements = measurements.setdefault(name, {})
+    for measurement_time, cmdline, cpu_percent in process_query:
+        cpu_measurements = measurements.setdefault(cmdline, {})
         cpu_measurements[measurement_time] = cpu_percent
     chart = pygal.Line(
         x_label_rotation=20,
         x_labels_major_count=10,
         show_minor_x_labels=False,
+        show_dots=False,
+        legend_at_bottom=True,
+        legend_at_bottom_columns=1,
     )
-    chart.x_labels = [
-        measurement_time.isoformat()
-        for measurement_time
-        in measurement_times
+    step_sizes = get_step_sizes(len(measurement_times), steps)
+    mid_points = [
+        sum(step_sizes[:i]) + int(step_sizes[i]/2)
+        for i in range(len(step_sizes))
     ]
-    for name, cpu_measurements in measurements.items():
+    chart.x_labels = [
+        measurement_times[i].isoformat()
+        for i in mid_points
+    ]
+    for cmdline, cpu_measurements in measurements.items():
+        data = [
+            cpu_measurements.get(measurement_time, 0)
+            for measurement_time
+            in measurement_times
+        ]
+        chopped_data = chop_list(data, step_sizes)
+        modes = [
+            get_mode(part, 2)
+            for part in chopped_data
+        ]
         chart.add(
-            '{}'.format(name),
-            [
-                cpu_measurements.get(measurement_time, None)
-                for measurement_time
-                in measurement_times
-            ],
+            '{}'.format(cmdline),
+            modes,
             allow_interruptions=True,
         )
     chart.render_to_file('./process_chart.svg')
+    chart.render_to_png('./process_chart.png')
 
 
 def run():
     Session = sessionmaker(bind=engine)
     session = Session()
     measurement_times = get_measurement_times(session)
-    create_cpu_chart(session, measurement_times)
-    create_process_chart(session, measurement_times)
+    steps = 400
+    create_cpu_chart(session, measurement_times, steps)
+    create_process_chart(session, measurement_times, steps)
 
 
 if __name__ == '__main__':
